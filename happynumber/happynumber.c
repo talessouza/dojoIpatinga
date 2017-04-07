babb28276f650849a0acc2788cd5a8558c20cbd6
Tales Wallace Souza <tales.wallace@gmail.com>
2017-04-07 19:53:56 -0300

    Commit inicial do Dojo Ipatinga

---
+++ happynumber/happynumber.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio2.h>
+
+
+void assertEquals(char * esperado, char * atual, char * mensagem) {
+	int resultado_comparacao = strcmp(esperado, atual);
+	if (resultado_comparacao == 0)
+	{
+		textcolor(LIGHTGREEN);
+		printf("%s - TEST OK! !!!\n", mensagem);
+		textcolor(LIGHTGRAY);
+	} else 
+	{
+		textcolor(LIGHTRED);
+		printf("%s - ****ERRO**** NO TEST - Esperado: [%s] retornado: [%s]!!!\n", mensagem, esperado, atual);
+		textcolor(LIGHTGRAY);
+	}
+}
+
+char * happy(int number) {
+	return "feliz";
+}
+
+
+int main(int argc, char *argv[]) {
+	
+	assertEquals("feliz", happy(1), "test 1");
+	
+	return 0;
+}
+
+
