6651654853c7cddeb8efa37c2cf662e8bceec07c
Tales Wallace Souza <tales.wallace@gmail.com>
2017-04-07 21:02:23 -0300

    Teste 10 e 100 - OK-Andréia

--- happynumber/happynumber.c
+++ happynumber/happynumber.c
@@ -5,13 +5,11 @@
 
 void assertEquals(char * esperado, char * atual, char * mensagem) {
 	int resultado_comparacao = strcmp(esperado, atual);
-	if (resultado_comparacao == 0)
-	{
+	if (resultado_comparacao == 0) {
 		textcolor(LIGHTGREEN);
 		printf("%s - TEST OK! !!!\n", mensagem);
 		textcolor(LIGHTGRAY);
-	} else 
-	{
+	} else {
 		textcolor(LIGHTRED);
 		printf("%s - ****ERRO**** NO TEST - Esperado: [%s] retornado: [%s]!!!\n", mensagem, esperado, atual);
 		textcolor(LIGHTGRAY);
@@ -19,21 +17,23 @@ void assertEquals(char * esperado, char * atual, char * mensagem) {
 }
 
 char * happy(int number) {
-	if (number == 1) {
-	return "feliz";}
-	if (number == 4) {
-	return "triste";
+	if (number == 1 || number == 10 || number == 100 ) {
+		return "feliz";
 	}
+		return "triste";
 	
-	
+
+
 }
 
 
 int main(int argc, char *argv[]) {
-	
+
 	assertEquals("feliz", happy(1), "test 1");
 	assertEquals("triste", happy(4), "test 4");
-		
+	assertEquals("feliz", happy(10), "test 10");
+	assertEquals("feliz", happy(100), "test 100");
+	
 	return 0;
 }
 
